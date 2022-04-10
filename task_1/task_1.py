5f9cd302c0733842ccbdaec3fc76c5a8c4fa2820
Nadezhda <nadvlasova@rambler.ru>
2022-04-10 12:58:04 +0800

    остался вопрос по функции в первом пункте

--- task_1/task_1.py
+++ task_1/task_1.py
@@ -16,15 +16,31 @@
 word1 = "разработка"
 word2 = "сокет"
 word3 = "декоратор"
+all_words = [word1, word2, word3]
 
-all_world = (word1, word2, word3)
-for i in all_world:
-    print(i.encode())
-    print(type(i))
+# Uword1 = b'\xd1\x80\xd0\xb0\xd0\xb7\xd1\x80\xd0\xb0\xd0\xb1\xd0\xbe\xd1\x82\xd0\xba\xd0\xb0'
+# Uword2 = b'\xd1\x81\xd0\xbe\xd0\xba\xd0\xb5\xd1\x82'
+# Uword3 = b'\xd0\xb4\xd0\xb5\xd0\xba\xd0\xbe\xd1\x80\xd0\xb0\xd1\x82\xd0\xbe\xd1\x80'
+#
+# all_Uwords = (Uword1, Uword2, Uword3)
+# for word in all_Uwords:
+#     print(word)
+#     print(type(word))
 
 # b'\xd1\x80\xd0\xb0\xd0\xb7\xd1\x80\xd0\xb0\xd0\xb1\xd0\xbe\xd1\x82\xd0\xba\xd0\xb0'
-# <class 'str'>
+# <class 'bytes'>
 # b'\xd1\x81\xd0\xbe\xd0\xba\xd0\xb5\xd1\x82'
-# <class 'str'>
+# <class 'bytes'>
 # b'\xd0\xb4\xd0\xb5\xd0\xba\xd0\xbe\xd1\x80\xd0\xb0\xd1\x82\xd0\xbe\xd1\x80'
-# <class 'str'>
+# <class 'bytes'>
+
+for word in all_words:
+    print(f"'{word}' - буквенный формат - {type(word)}")
+    print(f"'{word.encode('unicode_escape').decode('utf-8')}' - набор кодовых точек - {type(word)}")
+
+# 'разработка' - буквенный формат - <class 'str'>
+# '\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430' - набор кодовых точек - <class 'str'>
+# 'сокет' - буквенный формат - <class 'str'>
+# '\u0441\u043e\u043a\u0435\u0442' - набор кодовых точек - <class 'str'>
+# 'декоратор' - буквенный формат - <class 'str'>
+# '\u0434\u0435\u043a\u043e\u0440\u0430\u0442\u043e\u0440' - набор кодовых точек - <class 'str'>
